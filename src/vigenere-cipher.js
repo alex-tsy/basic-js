712a7d971e427a769d1582b97c456980b2a0b4f2
alex-tsy <68004960+alex-tsy@users.noreply.github.com>
2020-09-29 13:54:21 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(order = true) {
+        this.reverse = !order;
+    }
+
+    cipher(message, key, func) {
+        if (message == undefined || key == undefined) {
+            throw new Error();
+        }
+
+        let enc = '';
+        let keyIndex = 0;
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        for (let i = 0; i < message.length; i++) {
+            let currentKey = key[keyIndex % key.length];
+            if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+                let encCode = func(message.charCodeAt(i), currentKey);
+                enc += String.fromCharCode(encCode);
+                keyIndex++;
+            }
+            else {
+                enc += message[i];
+            }
+        }
+        return this.reverse ? enc.split('').reverse().join('') : enc;
+    }
+
+    enc(currentSymbol, currentKey) {
+        return (currentSymbol + currentKey.charCodeAt(0)) % 26 + 65;
+    }
+
+    dec(currentSymbol, currentKey) {
+        return (currentSymbol + 26 - currentKey.charCodeAt(0)) % 26 + 65;
+    }
+
+    encrypt(message, key) {
+        return this.cipher(message, key, this.enc);
+    }
+
+    decrypt(message, key) {
+        return this.cipher(message, key, this.dec)
+    }
 }
 
 module.exports = VigenereCipheringMachine;
